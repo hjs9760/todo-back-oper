36ba200cec245156cf82a2b7a82995dfc1ac7bb0
hoxx <haru9760@naver.com>
2020-12-04 15:13:44 +0900

    todolist 프로젝트 git 연동

---
+++ src/test/java/com/js/ms/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.ms.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
