6a979d7156cbe9f3a2e8be3cd0c52ec8a9f2fbdf
hoxx <haru9760@naver.com>
2021-04-19 16:39:11 +0900

    feat: 설정파일 암호화 (jasypt)

--- src/main/java/com/js/ms/todo/TodoApplication.java
+++ src/main/java/com/js/ms/todo/TodoApplication.java
@@ -1,13 +1,21 @@
 package com.js.ms.todo;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword("todoEncrypt"); //JasyptConfig 설정의 암호화 키
+    }
 }
---
+++ src/main/java/com/js/ms/todo/global/config/security/JasyptConfig.java
@@ -0,0 +1,25 @@
+package com.js.ms.todo.global.config.security;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+    @Bean("jasyptEncryptor")
+    public StringEncryptor stringEncryptor() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        encryptor.setPassword("todoEncrypt"); //암호화에 사용할 키
+        encryptor.setAlgorithm("PBEWithMD5AndDES"); //사용할 알고리즘
+//        config.setKeyObtentionIterations("1000");
+//        config.setPoolSize("1");
+//        config.setProviderName("HJS");
+//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+//        config.setStringOutputType("base64");
+//        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
